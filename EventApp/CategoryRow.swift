08705c8714f7436d965cb10db56f9f70ddd22d7d
Luisa Mello <lu_mello93@hotmail.com>
2016-02-24 16:41:50 -0300

    Começa a modificar da onde vem os dados para a collection view

--- EventApp/CategoryRow.swift
+++ EventApp/CategoryRow.swift
@@ -26,9 +26,6 @@ class CategoryRow: UITableViewCell, UICollectionViewDataSource, UICollectionView
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        arr=["1.png","2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png"]
-        
-
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
--- EventApp/MainScreen.swift
+++ EventApp/MainScreen.swift
@@ -13,8 +13,7 @@ class MainScreen: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var table: UITableView!
     
-    
-    
+    var dados = ["1.png","2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
